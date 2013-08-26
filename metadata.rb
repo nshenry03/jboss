d368146889fe80e9b12c922cc0ae09f02100d2fc
Nick Henry <nickh@standingcloud.com>
2013-08-26 17:21:48 -0600

    first commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'jboss'
+maintainer       'Standing Cloud'
+maintainer_email 'cookbooks@standingcloud.com'
+license          'Apache 2.0'
+description      'Installs/Configures jboss'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
