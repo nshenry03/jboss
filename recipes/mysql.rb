27c03b95bd3545e674645ce51659d3d4c9dd771d
Nick Henry <nshenry03@gmail.com>
2013-09-18 11:18:31 -0600

    Add support to install the MySQL Connector for JBoss

---
+++ recipes/mysql.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: jboss
+# Recipe:: mysql
+# Description:: Use MySQL as the Default DataSource
+#
+# Copyright 2013, Standing Cloud
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe 'mysql_connector'
+mysql_connector_j "#{node['jboss']['jboss_home']}/server/default/lib"
